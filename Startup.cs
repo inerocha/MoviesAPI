503233d5748f0c92afb3d54c2396aa18b92c1972
Ingrid Rocha <97315541+rochaao@users.noreply.github.com>
2022-07-29 12:40:48 -0300

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -1,42 +1,38 @@
+using MovieAPI.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using MoviesAPI.Data;
-using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace MoviesAPI
+namespace MovieAPI.Controllers
 {
-
     public class Startup
     {
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration; 
+            Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
-        //This method gets called by the runtime. Use this method to add services to the container.
-
+        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<MovieContext>(options => options.UseMySQL(Configuration.GetConnectionString("MovieConnection")));
+            var serverVersion = new MySqlServerVersion(new Version(8, 0, 29));
+
+            services.AddDbContext<MovieContext>(options => options.UseMySql(Configuration.GetConnectionString("MovieConnection"), serverVersion));
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "MoviesAPI", Version = "v1" });
+            });
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies()); 
         }
 
-        //This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -45,13 +41,15 @@ namespace MoviesAPI
             }
 
             app.UseHttpsRedirection();
-            app.UseRouting(); 
+
+            app.UseRouting();
+
             app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
             });
         }
-
     }
 }
