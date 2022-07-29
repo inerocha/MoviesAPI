7b5de7d3e87ff6a1f748cd2cc70de76ac418e362
Ingrid Rocha <97315541+rochaao@users.noreply.github.com>
2022-07-29 09:21:43 -0300

    Create MovieProfiles.cs

---
+++ Profiles/MovieProfiles.cs
@@ -0,0 +1,21 @@
+using AutoMapper;
+using MovieAPI.Data.Dto;
+using MovieAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieAPI.Profiles
+{
+    public class MovieProfile : Profile
+    {
+        public MovieProfile()
+        {
+            CreateMap<CreateMovieDto, Movie>() ;
+            CreateMap<Movie, ReadMovieDto>();
+            CreateMap<UpdateMovieDto, Movie>();
+
+        }
+    }
+}
