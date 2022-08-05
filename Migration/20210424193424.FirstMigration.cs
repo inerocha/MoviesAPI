98fee6ed41b4eac4e754853bca7d233d50f9a80b
Ingrid Rocha <97315541+inerocha@users.noreply.github.com>
2022-08-05 14:49:58 -0300

    Create 20210424193424.FirstMigration.cs

---
+++ Migration/20210424193424.FirstMigration.cs
@@ -0,0 +1,34 @@
+using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+namespace MoviesAPI.Migrations
+{
+    public partial class FirstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Movies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    Duration = table.Column<int>(type: "int", nullable: false),
+                    Director = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: true),
+                    Genre = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movies", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Movies");
+        }
+    }
+}
